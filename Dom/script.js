e8f98f231b826abb3ba788c04c8aceb33ddf062c
Prem99 <prem97010@gmail.com>
2023-02-14 18:22:49 +0530

    practise problems javascript

---
+++ Dom/script.js
@@ -0,0 +1,12 @@
+
+ function green() {
+    let element = document.getElementById("box");
+    element.style.background = "lightgreen";
+    element.innerHTML = "Paragraph has lightgreen background";
+  }
+  function pink() {
+    let element = document.getElementById("box");
+    element.style.background = "lightpink";
+    element.innerHTML = "Paragraph has lightpink background";
+  }
+ 
