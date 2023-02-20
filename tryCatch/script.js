3345e8edf3ff90365966ca60c24d7112cdfa07b5
Prem-99 <115132125+Prem-99@users.noreply.github.com>
2023-02-20 17:48:36 +0530

    Add files via upload

---
+++ tryCatch/script.js
@@ -0,0 +1,11 @@
+try {
+    console.log('start of try runs here');
+    unicycle;
+    console.log('End of try runs here');
+} catch(err){
+    console.log('Error has occured:' + err + );
+   
+}
+
+
+console.log('...then the excution continues');
